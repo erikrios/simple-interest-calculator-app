1685bcc870dca96c6b7d36d0f8f16ea12e4420f0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-11 11:54:25 +0700

    Add MaterialApp and Scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,26 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
+    return MaterialApp(
+      title: 'Simple Interest Calculator',
+      home: SimpleInterestCalculator(),
+      theme: ThemeData(),
+    );
+  }
+}
+
+class SimpleInterestCalculator extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SimpleInterestCalculatorState();
+  }
+}
+
+class _SimpleInterestCalculatorState extends State<SimpleInterestCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
   }
 }
