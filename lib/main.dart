ca6ebd164c0413fd2ae4f9a8c506b66010bfa380
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-11 12:28:48 +0700

    Add TextField widget

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class SimpleInterestCalculator extends StatefulWidget {
 }
 
 class _SimpleInterestCalculatorState extends State<SimpleInterestCalculator> {
-  final double _minimumPadding = 10.0;
+  final double _minimumPadding = 5.0;
 
   @override
   Widget build(BuildContext context) {
@@ -35,25 +35,62 @@ class _SimpleInterestCalculatorState extends State<SimpleInterestCalculator> {
       appBar: AppBar(
         title: Text('Simple Interest Calculator'),
       ),
-      body: ListView(
-        children: <Widget>[
-          _getCalculatorLogo(),
-        ],
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        child: ListView(
+          children: <Widget>[
+            _getCalculatorLogo(),
+            _getTextField(
+              labelText: 'Principal',
+              hintText: 'Enter Principal e.g. 12000',
+            ),
+            _getTextField(
+              labelText: 'Rate of interest',
+              hintText: 'In percent',
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: _minimumPadding,
+                bottom: _minimumPadding,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 
   Widget _getCalculatorLogo() {
     AssetImage assetImage = AssetImage('images/money.png');
-    Image image = Image(image: assetImage);
+    Image image = Image(
+      image: assetImage,
+      width: 125.0,
+      height: 125.0,
+    );
     return Container(
-      padding: EdgeInsets.only(
-        top: _minimumPadding * 5,
-        bottom: _minimumPadding * 5,
+      margin: EdgeInsets.all(
+        _minimumPadding * 5,
       ),
-      width: 250,
-      height: 250,
       child: image,
+    );
+  }
+
+  Widget _getTextField({String labelText, String hintText}) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: _minimumPadding,
+        bottom: _minimumPadding,
+      ),
+      child: TextField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          labelText: labelText,
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+      ),
     );
   }
 }
