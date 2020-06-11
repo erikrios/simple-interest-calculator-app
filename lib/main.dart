9f178a6a74a3d0f27e2f28ab7b1928beb9569bc0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-11 12:06:34 +0700

    Add Image widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Simple Interest Calculator',
       home: SimpleInterestCalculator(),
-      theme: ThemeData(),
+      theme: ThemeData(
+        primaryColor: Colors.indigo,
+        accentColor: Colors.indigoAccent,
+      ),
     );
   }
 }
@@ -23,10 +27,33 @@ class SimpleInterestCalculator extends StatefulWidget {
 }
 
 class _SimpleInterestCalculatorState extends State<SimpleInterestCalculator> {
+  final double _minimumPadding = 10.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Interest Calculator'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _getCalculatorLogo(),
+        ],
+      ),
+    );
+  }
 
+  Widget _getCalculatorLogo() {
+    AssetImage assetImage = AssetImage('images/money.png');
+    Image image = Image(image: assetImage);
+    return Container(
+      padding: EdgeInsets.only(
+        top: _minimumPadding * 5,
+        bottom: _minimumPadding * 5,
+      ),
+      width: 250,
+      height: 250,
+      child: image,
     );
   }
 }
